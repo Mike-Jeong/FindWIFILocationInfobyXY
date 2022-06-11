5a61c8f30f26432f8cae26759b42bae11aff2d70
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-06-11 14:56:02 +0900

    db connection util 수정

---
+++ src/main/java/DB/DBConnUtils.java
@@ -0,0 +1,47 @@
+package DB;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConnUtils {
+
+    public static Connection getDBCP() {
+        Connection conn = null;
+        String jndiName = "jdbc/mysql";
+        try {
+            Context initContext = (Context) new InitialContext().lookup("java:/comp/env");
+            DataSource ds = (DataSource) initContext.lookup(jndiName);
+            conn = ds.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+
+    public static void closeConnection(Connection conn, Statement stmt, ResultSet rs, String name) {
+        if (rs != null) try {
+            rs.close();
+            System.out.println(name + " rs close");
+        } catch (SQLException ex) {
+            System.out.println(name + " rs error");
+        }
+        if (stmt != null) try {
+            stmt.close();
+            System.out.println(name + " stmt close");
+        } catch (SQLException ex) {
+            System.out.println(name + " stmt error");
+        }
+        if (conn != null) try {
+            conn.close();
+            System.out.println(name + " conn close");
+        } catch (SQLException ex) {
+            System.out.println(name + " conn error");
+        }
+    }
+}
